0f6c74d5228e5cf5756e6211f2ed6cb1810e1700
Piotr Muszel <pmuszel@gmail.com>
2020-12-11 15:13:45 +0100

    Init repo

---
+++ graphql/resolvers.js
@@ -0,0 +1,150 @@
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+const Post = require('../models/post');
+
+module.exports = {
+  createUser: async function ({ userInput }, req) {
+    const email = userInput.email;
+    const password = userInput.password;
+    const name = userInput.name;
+
+    const errors = [];
+    if (!validator.isEmail(email)) {
+      errors.push({ message: 'Invalid email!' });
+    }
+
+    if (
+      validator.isEmpty(password) ||
+      !validator.isLength(password, { min: 5 })
+    ) {
+      errors.push({ message: 'Invalid password!' });
+    }
+
+    if (errors.length > 0) {
+      const error = new Error('Invalid input');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+      const error = new Error('User exists!');
+      throw error;
+    }
+
+    const hashedPw = await bcrypt.hash(password, 12);
+
+    const user = new User({
+      email: email,
+      password: hashedPw,
+      name: name,
+    });
+    const result = await user.save();
+
+    return { ...result._doc, _id: result._id.toString() };
+  },
+
+  login: async function({ email, password }) {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const error = new Error('User not found.');
+      error.code = 401;
+      throw error;
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      const error = new Error('Password is incorrect.');
+      error.code = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        email: user.email
+      },
+      'somesupersecretsecret',
+      { expiresIn: '1h' }
+    );
+    return { token: token, userId: user._id.toString() };
+  },
+  createPost: async function({ postInput }, req) {
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated!');
+      error.code = 401;
+      throw error;
+    }
+    const errors = [];
+    if (
+      validator.isEmpty(postInput.title) ||
+      !validator.isLength(postInput.title, { min: 5 })
+    ) {
+      errors.push({ message: 'Title is invalid.' });
+    }
+    if (
+      validator.isEmpty(postInput.content) ||
+      !validator.isLength(postInput.content, { min: 5 })
+    ) {
+      errors.push({ message: 'Content is invalid.' });
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid input.');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    const user = await User.findById(req.userId);
+    if (!user) {
+      const error = new Error('Invalid user.');
+      error.code = 401;
+      throw error;
+    }
+    const post = new Post({
+      title: postInput.title,
+      content: postInput.content,
+      imageUrl: postInput.imageUrl,
+      creator: user
+    });
+    const createdPost = await post.save();
+    user.posts.push(createdPost);
+    await user.save();
+    return {
+      ...createdPost._doc,
+      _id: createdPost._id.toString(),
+      createdAt: createdPost.createdAt.toISOString(),
+      updatedAt: createdPost.updatedAt.toISOString()
+    };
+  },
+  posts: async function({ page }, req) {
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated!');
+      error.code = 401;
+      throw error;
+    }
+    if (!page) {
+      page = 1;
+    }
+    const perPage = 2;
+    const totalPosts = await Post.countDocuments();
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .populate('creator');
+    return {
+      posts: posts.map(p => {
+        return {
+          ...p._doc,
+          _id: p._id.toString(),
+          createdAt: p.createdAt.toISOString(),
+          updatedAt: p.updatedAt.toISOString()
+        };
+      }),
+      totalPosts: totalPosts
+    };
+  }
+};
